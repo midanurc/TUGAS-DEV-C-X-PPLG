22a23db6a83dfe82e2d7524e5b9e703a66f6a0d0
midanurc <113220318+midanurc@users.noreply.github.com>
2022-09-10 10:34:47 +0700

    Add files via upload
    nama:mida nur cahyaningsih
    kelas: X PPLG
    no.absen:22

---
+++ tugas di tools dev c.cpp
@@ -0,0 +1,40 @@
+#include <iostream> 
+using namespace std; 
+int main(){
+	int jam, sewa;
+	
+	cout<<"Berapa lama akan meminjam?"<< endl;
+	cin >> jam;
+if(jam == 1) {
+	sewa = 50000 * jam + 50000;
+	cout << "Biaya sewanya adalah : " << sewa << endl;
+} else if ( jam == 2) {
+		sewa = 50000 * jam + 50000;
+	cout << "Biaya sewanya adalah : " << sewa << endl;
+} else if ( jam == 3) {
+		sewa = 50000 * jam + 50000;
+	cout << "Biaya sewanya adalah : " << sewa << endl;
+} else if ( jam == 4) {
+		sewa = 50000 * jam + 50000;
+	cout << "Biaya sewanya adalah : " << sewa << endl;
+}  else if ( jam == 5) {
+		sewa = 50000 * jam + 50000;
+	cout << "Biaya sewanya adalah : " << sewa << endl;
+} else if ( jam == 6) {
+	sewa = 50000 * jam + 50000;
+	cout << "Biaya sewanya adalah : " << sewa << endl;
+} else if ( jam == 8) {
+	sewa = 50000 * jam + 50000;
+	cout << "Biaya sewanya adalah : " << sewa << endl;
+} else if ( jam == 9) { 
+	sewa = 50000 * jam + 50000;
+	cout << "Biaya sewanya adalah : " << sewa << endl;
+}  else if ( jam == 10) {
+sewa = 50000 * jam + 50000;
+	cout << "Biaya sewanya adalah : " << sewa << endl;
+}  else if ( jam > 10) {
+sewa = 50000 * jam + 50000;
+	cout << "Menyewa kah?" << endl;
+} 
+    return 0; 
+}
